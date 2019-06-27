4611d9bff88ca230ab2548c530ce8983c0e1fb9e
Martin Strauss <martin@ockle.org>
2019-06-27 14:04:50 -0700

    clean start

--- README.md
+++
@@ -1,8 +0,0 @@
-# noninstantcamera
-
-A decidedly not instant camera.
-
-## Getting Started
-
-For help getting started with Flutter, view our online
-[documentation](http://flutter.io/).
--- android/app/src/main/java/au/id/martinstrauss/noninstantcamera/noninstantcamera/ShareFileProvider.java
+++
@@ -1,5 +0,0 @@
-package au.id.martinstrauss.noninstantcamera.noninstantcamera;
-
-import android.support.v4.content.FileProvider;
-
-public class ShareFileProvider extends FileProvider {}
